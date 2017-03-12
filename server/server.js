f2a079258c743ebfd4f422f4f5a13a2d0ed61553
Mohamed Al-Rifay <mohamed.abdelattif@fci.helwan.edu.eg>
2017-03-12 21:26:08 +0200

    add user model

--- server/server.js
+++ server/server.js
@@ -5,21 +5,48 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 const Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: [true, 'Where is your todo?'],
+        minlength: [5, 'To few characters!'],
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-const myTodo = new Todo({
-    text: 'Mohamed',
-    completed: false,
-    completedAt: new Date().getUTCDate()
+const User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: [true, 'Where is your email?'],
+        minlength: [5, 'Email is not correct!'],
+        validate: {
+            validator(email) {
+                return /[a-zA-z]\w*@\w+\.\w+/.test(email);
+            },
+            msg: 'Email shit'
+        },
+        trim: true
+    }
 });
-
-myTodo.save().then(doc => console.log('todo saved', doc), error => console.log('error saving todo', error));
-
+// const myTodo = new Todo({
+//     text: ' g  '
+// });
+// myTodo.save().then(doc => {
+//     console.log('todo saved', doc);
+// }, error => {
+//     console.log('error saving todo:', error.errors.text.message);
+// });
+const myUser = new User({
+    email: 'X@cdd.d                '
+});
+myUser.save().then(doc => {
+    console.log('User saved', doc);
+}, error => {
+    console.log('error saving user:', error.errors.email.message);
+});
